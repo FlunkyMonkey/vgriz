83e7ba70f9b881101356bdcde43132b13f1aab9c
FlunkyMonkey <Mike.Beil1@gmail.com>
2024-07-03 21:57:20 -0700

    containerd setup

--- build.py
+++ build.py
@@ -29,3 +29,49 @@ echo "fs.nr_open = 1048599" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 #start and enable haproxy
+sudo systemctl enable --now haproxy
+
+#disable swap on kubernetes cluster nodes
+sudo swapoff -a #Only did Master
+sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+#Enable Kernel IP forwarding on Cluster Nodes
+echo "net.ipv4.ip_forward=1" | sudo tee -a  /etc/sysctl.conf
+sudo sysctl -p
+
+#Load Some Required Kernel Modules on Cluster Nodes
+echo 'overlay
+br_netfilter' | sudo tee /etc/modules-load.d/kubernetes.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo lsmod | grep -E "overlay|br_netfilter"
+sudo tee -a /etc/sysctl.conf << 'EOL'
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOL
+sudo sysctl -p
+
+#Install Container Runtime on Cluster Nodes
+sudo apt install apt-transport-https \
+	ca-certificates curl \
+	gnupg-agent \
+	software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -sc) stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+
+sudo apt update
+
+sudo apt install -y containerd.io
+
+#Configure Cgroup Driver for ContainerD
+[ -d /etc/containerd ] || sudo mkdir /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i '/SystemdCgroup/s/false/true/' /etc/containerd/config.toml
+
+sudo sed -i '/pause:3.8/s/3.8/3.9/' /etc/containerd/config.toml
+grep sandbox_image /etc/containerd/config.toml
+sudo systemctl enable --now containerd
+systemctl status containerd
